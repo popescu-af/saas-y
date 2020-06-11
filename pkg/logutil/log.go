06543f52f537b39d58fda08443228b18c23d020f
Alexandru Popescu <popescu.af@gmail.com>
2020-06-11 21:27:40 +0200

    Add logger utility lib.

---
+++ pkg/logutil/log.go
@@ -0,0 +1,49 @@
+package logutil
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var logger = newLogger()
+
+func newLogger() *zap.Logger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("failed to initialize logger - %v", err)
+	}
+	return logger
+}
+
+// Sync flushes the log cache.
+func Sync() {
+	if err := logger.Sync(); err != nil {
+		log.Error("failed to flush log cache - %v", err)
+	}
+}
+
+// Debug logs a message with DEBUG level.
+func Debug(msg string, fields ...Field) {
+	logger.Debug(msg, fields...)
+}
+
+// Error logs a message with ERROR level.
+func Error(msg string, fields ...Field) {
+	logger.Error(msg, fields...)
+}
+
+// Fatal logs a message with FATAL level.
+func Fatal(msg string, fields ...Field) {
+	logger.Fatal(msg, fields...)
+}
+
+// Info logs a message with INFO level.
+func Info(msg string, fields ...Field) {
+	logger.Info(msg, fields...)
+}
+
+// Warn logs a message with WARN level.
+func Warn(msg string, fields ...Field) {
+	logger.Warn(msg, fields...)
+}
