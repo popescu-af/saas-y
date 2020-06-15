b379c4f5e3c2904f0e8453fba854ddfbf0c18332
Alexandru Popescu <popescu.af@gmail.com>
2020-06-15 12:53:13 +0200

    Better names.

--- pkg/connection/fullduplex.go
+++ pkg/connection/fullduplex.go
@@ -6,16 +6,20 @@ import (
 	"time"
 )
 
-// ReaderFn is the type for functions listening for messages.
-type ReaderFn func() (*Message, error)
+// ReadFn is the type of function listening for messages on a channel.
+type ReadFn func() (*Message, error)
 
-// WriterFn is the type for functions sending messages.
-type WriterFn func(*Message) error
+// WriteFn is the type of function sending messages on a channel.
+type WriteFn func(*Message) error
+
+// CloseFn is the type of function closing a channel.
+type CloseFn func(*Message) error
 
 // Channel is the structure containing a channel's read/write functions.
 type Channel struct {
-	Read  ReaderFn
-	Write WriterFn
+	Read  ReadFn
+	Write WriteFn
+	Close CloseFn
 }
 
 // Message is a structure encompassing a message's type and payload.
@@ -33,17 +37,16 @@ type Message struct {
 type FullDuplexEndpoint interface {
 	// ProcessMessage should process the given message and react accordingly,
 	// by changing state and/or writing something back or none of them.
-	ProcessMessage(*Message, WriterFn) error
+	ProcessMessage(*Message, WriteFn) error
 	// Poll should inspect the current state and decide whether to take any action
 	// and/or write something on the channel or not.
-	Poll(time.Time, WriterFn) error
+	Poll(time.Time, WriteFn) error
 }
 
-// HandleTwoWayConnection is a blocking function that takes a full-duplex endpoint
-// and a channel given by its read/write functions and handles messages arriving on
-// the channel as well as periodically polling the endpoint for new messages to be
-// dispached on the channel.
-func HandleTwoWayConnection(endpoint FullDuplexEndpoint, channel Channel, pollingPeriod time.Duration) {
+// TalkFullDuplex is a blocking function that takes a full-duplex endpoint
+// and a channel. It handles messages arriving on the channel while periodically
+// polling the endpoint for new messages to be dispached on the channel.
+func TalkFullDuplex(endpoint FullDuplexEndpoint, channel Channel, pollingPeriod time.Duration) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
@@ -64,6 +67,8 @@ func HandleTwoWayConnection(endpoint FullDuplexEndpoint, channel Channel, pollin
 		}
 	}()
 
+	var finalError error
+
 	// processor
 	go func() {
 		defer wg.Done()
@@ -79,6 +84,7 @@ func HandleTwoWayConnection(endpoint FullDuplexEndpoint, channel Channel, pollin
 				}
 			case err := <-errCh:
 				fmt.Printf("received error: %v", err)
+				finalError = err
 				return
 			case t := <-ticker.C:
 				if err := endpoint.Poll(t, channel.Write); err != nil {
@@ -90,4 +96,5 @@ func HandleTwoWayConnection(endpoint FullDuplexEndpoint, channel Channel, pollin
 	}()
 
 	wg.Wait()
+	return finalError
 }
