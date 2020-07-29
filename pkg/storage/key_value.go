78bd437db110d02a640602f460212cb8c7afcfb1
Alexandru Popescu <popescu.af@gmail.com>
2020-07-29 08:03:08 +0200

    Rename.

--- pkg/storage/key_value.go
+++ pkg/storage/key_value.go
@@ -2,8 +2,8 @@ package storage
 
 import "time"
 
-// KeyValueStore is the interface for a storage holding key-value pairs.
-type KeyValueStore interface {
+// KeyValue is the interface for a storage holding key-value pairs.
+type KeyValue interface {
 	Get(key string) (string, error)
 	Set(key string, value []byte, expiration time.Duration) error
 	Delete(key string) error
--- pkg/storage/redis.go
+++ pkg/storage/redis.go
@@ -7,30 +7,30 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-// RedisStorage is a wrapper over the redis client.
-type RedisStorage struct {
+// Redis is a wrapper over the redis client.
+type Redis struct {
 	client *redis.Client
 }
 
-// NewRedisStorage creates a new instance of Redis storage.
-func NewRedisStorage(options *redis.Options) *RedisStorage {
-	return &RedisStorage{
+// NewRedis creates a new instance of Redis storage.
+func NewRedis(options *redis.Options) *Redis {
+	return &Redis{
 		client: redis.NewClient(options),
 	}
 }
 
 // Get returns the pre-cached value for the given key.
-func (r *RedisStorage) Get(key string) (string, error) {
+func (r *Redis) Get(key string) (string, error) {
 	val, err := r.client.Get(context.TODO(), key).Result()
 	return val, err
 }
 
 // Set sets the value for the specified key in the cache.
-func (r *RedisStorage) Set(key string, value []byte, expiration time.Duration) error {
+func (r *Redis) Set(key string, value []byte, expiration time.Duration) error {
 	return r.client.Set(context.TODO(), key, value, expiration).Err()
 }
 
 // Delete removes the entry for the specified key.
-func (r *RedisStorage) Delete(key string) error {
+func (r *Redis) Delete(key string) error {
 	return r.client.Del(context.TODO(), key).Err()
 }
